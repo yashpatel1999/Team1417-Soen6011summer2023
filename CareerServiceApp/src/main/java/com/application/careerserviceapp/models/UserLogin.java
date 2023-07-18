1b176a7255718f5f7e3cecff39cd510bea713b38
yashpatel1999 <patelyc1999@gmail.com>
2023-07-17 21:59:27 -0400

    "Initial SpringBoot Setup for Career Service Application"

---
+++ CareerServiceApp/src/main/java/com/application/careerserviceapp/models/UserLogin.java
@@ -0,0 +1,68 @@
+package com.application.careerserviceapp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user_login")
+@Component
+public class UserLogin {
+    @Id
+    @Column(name="user_id",unique = true)
+    private String uid;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="type")
+    private String type;
+
+    @Column(name="approved")
+    private String approved;
+
+    public String toString() {
+        return uid + "  " + password + "  " + type;
+    }
+
+    public String getApproved() {
+        return approved;
+    }
+
+    public void setApproved(String approved) {
+        this.approved = approved;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
---
+++ CareerServiceApp/src/test/java/com/application/careerserviceapp/CareerServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.careerserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
